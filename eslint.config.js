82455731d7b55af247d1b31be84a71039dda5b7c
Kazuhito Hokamura <k.hokamura@gmail.com>
2025-02-28 11:17:36 +0900

    build: initial project setup

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import eslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': eslint,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'no-debugger': 'error',
+    },
+  },
+];
